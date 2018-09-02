8c2ad6a987a432d32b707b768f74f72bee3f614e
lhzsdnu <2383376138@qq.com>
2018-09-02 16:38:21 +0800

    品优购  增加广告接口和广告服务

--- pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/ContentCategoryController.java
+++ pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/ContentCategoryController.java
@@ -2,10 +2,10 @@ package com.pinyougou.manager.controller;
 
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.pinyougou.content.service.ContentCategoryService;
 import com.pinyougou.entity.ContentCategory;
 import com.pinyougou.pojo.PageResult;
 import com.pinyougou.pojo.Result;
-import com.pinyougou.sellergoods.service.ContentCategoryService;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
