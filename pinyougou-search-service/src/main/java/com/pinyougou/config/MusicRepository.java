c590ab09098e726d87319f3c19b9b07c0ab7aee1
lhzsdnu <2383376138@qq.com>
2018-09-19 23:19:08 +0800

    solr  查询关键字高亮

--- pinyougou-search-service/src/main/java/com/pinyougou/config/MusicRepository.java
+++ pinyougou-search-service/src/main/java/com/pinyougou/config/MusicRepository.java
@@ -2,13 +2,26 @@ package com.pinyougou.config;
 
 import com.pinyougou.pojo.CopyItem;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.solr.core.query.result.HighlightPage;
 import org.springframework.data.solr.core.query.result.ScoredPage;
+import org.springframework.data.solr.repository.Highlight;
 import org.springframework.data.solr.repository.SolrCrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MusicRepository extends SolrCrudRepository<CopyItem, String> {
 
-    ScoredPage findByKeywordsEquals(String keywords,Pageable pageable);
+    ScoredPage findByKeywordsEquals(String keywords, Pageable pageable);
+
+    //设置高亮的域,高亮前缀,高亮后缀
+    @Highlight(prefix = "<em style='color:red'>", postfix = "</em>")
+    HighlightPage<CopyItem> findByContent(String content, Pageable pageable);
+
+    @Highlight(prefix = "<em style='color:red'>", postfix = "</em>")
+    HighlightPage<CopyItem> findByTitle(String title, Pageable pageable);
+
+    @Highlight(prefix = "<em style='color:red'>", postfix = "</em>")
+    HighlightPage<CopyItem> findByTitleOrContentLike(String title, String content, Pageable pageable);
+
 
 }
--- pinyougou-search-service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
+++ pinyougou-search-service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
@@ -9,7 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.solr.core.SolrTemplate;
-import org.springframework.data.solr.core.query.result.ScoredPage;
+import org.springframework.data.solr.core.query.result.HighlightEntry;
+import org.springframework.data.solr.core.query.result.HighlightPage;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
@@ -34,19 +35,37 @@ public class ItemSearchServiceImpl implements ItemSearchService {
     @Autowired
     private SolrTemplate solrTemplate;
 
+    private Map searchList(Map searchMap) {
+        Map<String, Object> map = new HashMap<>();
+
+        //按照关键字查询
+        String str = searchMap.get("keywords").toString();
+        Pageable pageable = PageRequest.of(0, 20);
+        //添加查询条件
+        HighlightPage<CopyItem> page = musicRepository.findByTitle(str, pageable);
+
+        //循环高亮入口集合
+        for (HighlightEntry<CopyItem> h : page.getHighlighted()) {
+            //获取原实体类
+            CopyItem item = h.getEntity();
+            //实际上此处是遍历的，但现只要求title高亮
+            if (h.getHighlights().size() > 0 && h.getHighlights().get(0).getSnipplets().size() > 0) {
+                //设置高亮的结果
+                item.setTitle(h.getHighlights().get(0).getSnipplets().get(0));
+            }
+        }
+
+        map.put("rows", page.getContent());
+        return map;
+    }
 
     @Override
     public Map<String, Object> search(Map searchMap) {
         Map<String, Object> map = new HashMap<>();
+        //putAll可以合并两个Map，只不过如果有相同的key那么用后面的覆盖前面的
 
-        Pageable pageable = PageRequest.of(0, 20);
-        //添加查询条件
-        ScoredPage<CopyItem> page = musicRepository.findByKeywordsEquals(searchMap.get("keywords").toString(),pageable);
-
-
-        map.put("rows", page.getContent());
-
+        //1.查询列表
+        map.putAll(searchList(searchMap));
         return map;
-
     }
 }
