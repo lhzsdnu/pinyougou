11db250ffec52899e7fd78a4688162c9d5015cd9
lhzsdnu <2383376138@qq.com>
2018-10-10 23:55:00 +0800

    品优购  删除无关代码

--- pinyougou-solr-util/src/main/java/com/pinyougou/solrutil/Application.java
+++ pinyougou-solr-util/src/main/java/com/pinyougou/solrutil/Application.java
@@ -1,9 +1,11 @@
 package com.pinyougou.solrutil;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(value = "com.pinyougou.mapper")
 public class Application {
 
     public static void main(String[] args) {
