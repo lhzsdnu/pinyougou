9803b9714ae8f4afeaaa8e7a5c4adba9206c7a42
lhzsdnu <2383376138@qq.com>
2018-10-14 17:40:53 +0800

    品优购  更新配置

---
+++ pinyougou-page-web/src/main/java/com/pinyougou/PinyougouPageWebApplication.java
@@ -0,0 +1,12 @@
+package com.pinyougou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinyougouPageWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PinyougouPageWebApplication.class, args);
+    }
+}
